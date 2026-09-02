cc62d878265037b0e76573e4acaa0c9bfa5a5705
artemsukhov <strangecodder@gmail.com>
2026-09-02 22:37:54 +0300

    feat

--- services/order-service/internal/model/model.go
+++ services/order-service/internal/model/model.go
@@ -3,6 +3,9 @@ package model
 import "github.com/google/uuid"
 
 type OrderProduct struct {
-	ProductId uuid.UUID
-	Count     int32
+	ProductId uuid.UUID `json:"product_id"`
+	Count     int64
+}
+
+type CreateProductsDto struct {
 }
